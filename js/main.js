73852cb139ec4577c8ef10767b6c606af3e77fa7
LetsGoMan <ilikeseik@gmail.com>
2022-01-07 09:11:13 +0300

    make next 5 blocks of page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $(function(){
+  $('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
 
+		fixedContentPos: false
+	});
+
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.png" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"> <img src="images/arrow-right.png" alt=""></button>',
+	});
+
+	$('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+	});
 });
