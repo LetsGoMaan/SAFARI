34fb34b7a7c5ae5662c0237a274dc2781320e045
LetsGoMan <ilikeseik@gmail.com>
2022-01-11 08:47:27 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -25,4 +25,7 @@ $(function(){
 			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
 		},
 	});
+	$('.menu__btn').on('click', function(){
+		$('.menu__list').toggleClass('menu__list--active')
+	});
 });
